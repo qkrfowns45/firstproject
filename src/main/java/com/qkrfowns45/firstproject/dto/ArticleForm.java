c31671aeb87fc55e7b7e252c6ecc0aa5ce894c03
qkrfowns45 <qkrfowns45@naver.com>
2022-01-02 22:53:49 +0900

    세번째 commit DB에서 데이터 조회까지 완료

--- src/main/java/com/qkrfowns45/firstproject/dto/ArticleForm.java
+++ src/main/java/com/qkrfowns45/firstproject/dto/ArticleForm.java
@@ -1,24 +1,17 @@
 package com.qkrfowns45.firstproject.dto;
 
 import com.qkrfowns45.firstproject.entity.Article;
+import lombok.AllArgsConstructor;
+import lombok.ToString;
 
+//롬복 어노테이션 코드 단축
+@ToString
+@AllArgsConstructor
 public class ArticleForm {
 
     private String title;
     private String content;
 
-    public ArticleForm(String title, String content){
-        this.title = title;
-        this.content = content;
-    }
-
-    @Override
-    public String toString() {
-        return "ArticleForm{" +
-                "title='" + title + '\'' +
-                ", content='" + content + '\'' +
-                '}';
-    }
 
     public Article toEntity() {
 
--- src/main/java/com/qkrfowns45/firstproject/entity/Article.java
+++ src/main/java/com/qkrfowns45/firstproject/entity/Article.java
@@ -1,11 +1,18 @@
 package com.qkrfowns45.firstproject.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
+@ToString
+@AllArgsConstructor
 @Entity//DB가 해당 객체를 인식 가능
+@NoArgsConstructor //디폴트 생성자 이게 있어야 디폴트값을 선언할수 있다.
 public class Article {
 
     @Id // 대표값을 지정
@@ -18,18 +25,4 @@ public class Article {
     @Column
     private String content;
 
-    public Article(Long id, String title, String content) {
-        this.id = id;
-        this.title = title;
-        this.content = content;
-    }
-
-    @Override
-    public String toString() {
-        return "Article{" +
-                "id=" + id +
-                ", title='" + title + '\'' +
-                ", content='" + content + '\'' +
-                '}';
-    }
 }
