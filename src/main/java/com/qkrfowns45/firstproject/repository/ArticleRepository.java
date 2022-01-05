12c27523799fbbd9931b175f9b1e7d0f4cebeaf8
qkrfowns45 <qkrfowns45@naver.com>
2022-01-05 22:41:31 +0900

    세번째 commit DB에서 데이터 조회까지 완료

--- src/main/java/com/qkrfowns45/firstproject/repository/ArticleRepository.java
+++ src/main/java/com/qkrfowns45/firstproject/repository/ArticleRepository.java
@@ -3,6 +3,9 @@ package com.qkrfowns45.firstproject.repository;
 import com.qkrfowns45.firstproject.entity.Article;
 import org.springframework.data.repository.CrudRepository;
 
-public interface ArticleRepository extends CrudRepository<Article,Long> {
+import java.util.ArrayList;
 
+public interface ArticleRepository extends CrudRepository<Article,Long> {
+    @Override
+    ArrayList<Article> findAll();
 }
