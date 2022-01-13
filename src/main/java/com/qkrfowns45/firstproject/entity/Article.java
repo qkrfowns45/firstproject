8a23c9913b887eb3601b7f9f2dc134b48df0508d
qkrfowns45 <qkrfowns45@naver.com>
2022-01-13 23:18:24 +0900

    DB내용을 console에서 볼 수 있게 설정 및 DB설정 수정

--- src/main/java/com/qkrfowns45/firstproject/entity/Article.java
+++ src/main/java/com/qkrfowns45/firstproject/entity/Article.java
@@ -5,20 +5,17 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @ToString
 @AllArgsConstructor
-@Entity//DB가 해당 객체를 인식 가능
+@Entity//DB가 해당 객체를 인식 가능 (해당 클래스로 테이블을 만든다)
 @NoArgsConstructor //디폴트 생성자 이게 있어야 디폴트값을 선언할수 있다.
 @Getter
 public class Article {
 
     @Id // 대표값을 지정
-    @GeneratedValue //자동 생성 어노테이션
+    @GeneratedValue(strategy = GenerationType.IDENTITY) //자동 생성 어노테이션 DB가 자동으로 생성
     private Long id;
 
     @Column
